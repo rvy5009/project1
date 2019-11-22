608d33bd33fe3af6e8b35cfd7ad4258c145046af
rvy5009 <rvy5009@Raymonds-MacBook-Pro.local>
2019-11-22 14:26:26 -0500

    proposal

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 console.log("hi")
-let stockUrl = "https://www.alphavantage.co/query?"
-"function=TIME_SERIES_INTRADAY & symbol=MSFT & interval=5min & apikey=demo"
 
-let apiKey = "N84W2MQHNHRK05UG"
+
+let stockApi = "N84W2MQHNHRK05UG"
+let stockUrl= `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=Z${stockApi}`
+
+
+
